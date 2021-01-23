e60172ea396ad12ae4d732e6ef03667419bf4ec0
Domagoj Zganec <domagoj.zganec1@gmail.com>
2021-01-23 09:37:01 +0100

    chore: Post CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,35 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+
 
 const main = async () => {
-    console.log(__dirname)
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
-    const post = orm.em.create(Post, {title: "Second post"})
-    await orm.em.persistAndFlush(post);
-    // const posts = await orm.em.find(Post, {});
-    // console.log(posts);
+    
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false,
+        }),
+        context: () => ({ em: orm.em })
+    });
+
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(4000, () => {
+        console.log("server started on localhost:4000")
+    })
 }
 
 main();
